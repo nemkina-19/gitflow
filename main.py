326a2731ae4086169534fda0b60a5a2f7975848b
iki1you <vovastr0v@yandex.ru>
2021-01-05 20:30:05 +0500

    v 0.55 заставка

--- main.py
+++ main.py
@@ -13,6 +13,37 @@ size = WIDTH, HEIGHT = width, height = 550, 550
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -78,7 +109,6 @@ class Player(pygame.sprite.Sprite):
         if self.floor:
             self.speed_y += GRAVITY
 
-
         self.rect = self.rect.move(0, -1 * self.speed_y)
         if pygame.sprite.spritecollide(self, flat, 0):
             self.rect = self.rect.move(0, self.speed_y)
@@ -89,7 +119,6 @@ class Player(pygame.sprite.Sprite):
 
         if self.grounded and JUMP and not self.floor:
             self.speed_y = -1 * JUMP_HEIGHT
-
 
         self.rect = self.rect.move(self.speed_x, self.speed_y)
 
@@ -123,6 +152,8 @@ flag = False
 JUMP = False
 MOVE_LEFT = False
 MOVE_RIGHT = False
+
+start_screen()
 
 while running:
     clock.tick(FPS)
